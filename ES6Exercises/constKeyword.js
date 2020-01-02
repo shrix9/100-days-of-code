ef27303b192fc973d6cfe66390864388ae354de8
Shrikant Naidu <shrikant.naidu@sap.com>
2020-01-02 21:40:26 +0530

    Completed arrow functions, default & rest params etc exercises.

---
+++ ES6Exercises/constKeyword.js
@@ -0,0 +1,10 @@
+function printManyTimes(str) {
+  "use strict";
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+}
+printManyTimes("#100DaysOfCode");
---
+++ ES6Exercises/mutableConstArray.js
@@ -0,0 +1,11 @@
+const S = [5, 7, 2];
+function editInPlace() {
+  'use strict';
+S[0] = 2;
+S[1] = 5;
+S[2] = 7;
+  
+console.log(S);
+  
+}
+editInPlace();
